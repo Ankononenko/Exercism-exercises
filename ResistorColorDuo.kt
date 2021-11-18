6f264fb4731896c1d53cf3cbd01e5d001ad34e17
163114 <88505337+163114@users.noreply.github.com>
2021-11-18 15:57:09 +0300

    Add files via upload

---
+++ ResistorColorDuo.kt
@@ -0,0 +1,24 @@
+enum class Color {
+    BLACK, BROWN, RED, ORANGE, YELLOW, GREEN, BLUE, VIOLET, GREY, WHITE
+}
+object ResistorColorDuo {
+    fun value(vararg colors: Color): Int {
+        var listToAddTo: List<String> = mutableListOf()
+        for (color in colors) {
+            listToAddTo = listToAddTo + when (color) {
+                Color.BLACK -> "0"
+                Color.BROWN -> "1"
+                Color.RED -> "2"
+                Color.ORANGE -> "3"
+                Color.YELLOW -> "4"
+                Color.GREEN -> "5"
+                Color.BLUE -> "6"
+                Color.VIOLET -> "7"
+                Color.GREY -> "8"
+                Color.WHITE -> "9"
+            }
+        }
+        val stringToReturn: String = (listToAddTo[0] + listToAddTo[1])
+        return stringToReturn.toInt()
+    }
+}
